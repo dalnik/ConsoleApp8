2a9aa6f0f4db1d1e1c41781631a417ea83af164f
dalnik61327@gmail.com <dalnik61327@gmail.com>
2019-12-17 12:09:49 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Book
+    {
+        //privat string name; // название  книги
+        //int author, hapters, pages; // автор, главы, страницы
+
+        public Book(string name, string author, string hapters, int pages)
+        {
+            this.name = name;
+            this.author = author;
+            this.hapters = hapters;
+            this.pages = pages;
+        }
+        {
+            public String Name { get => name; set => name = value; }
+            public String Author { get =>author; set => author = value; }
+            public String Hapters { get => hapters; set => hapters = value; }
+            public int Pages { get => pages; set => pages = (value > 0 ? value : 1; }
+        }
+        public override string ToString()
+        {
+            return $ "Name: {name}, Autor: {autor}, Hapter: {hapter}, 
+        }
+    }
+    }
+
+        
+       
+    
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Name = "Programming in C#";
+            book.author = "Tishchenko";
+            book.hapters = "";
+            book.pages = 1000;
+
+            Console.WriteLine(book.Name);
+            Console.WriteLine(book.author);
+            Console.WriteLine(book.hapters);
+            Console.WriteLine(book.pages);
+
+        }
+    }
+}
